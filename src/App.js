2e9a732cbbc2a55616c1678fd1222a15cb6c4546
Rohit <rk212300@outlook.com>
2023-11-30 22:43:09 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -1,60 +1,55 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
-import About from './components/About';
-import Alert from './components/Alert';
-import React, { useState } from 'react'
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-} from "react-router-dom";
+import "./App.css";
 
+import React, { useState } from "react";
+
+import Alert from "./components/Alert";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
 
 function App() {
-
-    const [mode, setMode] = useState('light');
+    const [mode, setMode] = useState("light");
 
     const [alert, setAlert] = useState(null);
 
     const showAlert = (message, type) => {
         setAlert({
             msg: message,
-            type: type
-        })
+            type: type,
+        });
 
         setTimeout(() => {
-            setAlert(null)
-        }, 2000)
-    }
+            setAlert(null);
+        }, 2000);
+    };
 
     const toggleMode = () => {
-        if (mode === 'light') {
-            setMode('dark');
-            document.body.style.backgroundColor = 'black';
+        if (mode === "light") {
+            setMode("dark");
+            document.body.style.backgroundColor = "black";
             showAlert("Dark mode has been enabled", "success");
-        }
-
-        else {
-            setMode('light');
-            document.body.style.backgroundColor = 'white';
+        } else {
+            setMode("light");
+            document.body.style.backgroundColor = "white";
             showAlert("Light mode has been enabled", "success");
         }
-    }
+    };
     return (
-        <>
-            <Router>
-                <Navbar title="Text Utility" aboutText="About TextUtility" mode={mode} toggleMode={toggleMode} />
+        <div>
+            <Navbar
+                title="Text Utility"
+                aboutText="About TextUtility"
+                mode={mode}
+                toggleMode={toggleMode}
+            />
 
-                <Alert alert={alert} />
-                <Routes>
-                    <Route path="/about" element={<About />} />
+            <Alert alert={alert} />
 
-                    <Route path="/" element={
-                        <TextForm showAlert={showAlert} heading="Enter text to analyze" mode={mode} />} />
-                </Routes>
-            </Router>
-        </>
+            <TextForm
+                showAlert={showAlert}
+                heading="Enter text to analyze"
+                mode={mode}
+            />
+        </div>
     );
 }
 
